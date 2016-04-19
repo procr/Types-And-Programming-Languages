d9aa43499dbcc6ac8f8718439206c2738e5759dc
Rong Chen <procr@qq.com>
2016-04-19 19:39:10 +0800

    update test.f

--- fulluntyped/test.f
+++ fulluntyped/test.f
@@ -1,27 +1,27 @@
 /* Examples for testing */
 
+/* for arith testing */
+
 true;
 if false then true else false; 
-
-x/;
-x;
-
-x = true;
-x;
-if x then false else x; 
-
-lambda x. x;
-(lambda x. x) (lambda x. x x); 
-
-{x=lambda x.x, y=(lambda x.x)(lambda x.x)}; 
-{x=lambda x.x, y=(lambda x.x)(lambda x.x)}.x; 
-
-"hello";
-
-timesfloat (timesfloat 2.0 3.0) (timesfloat 4.0 5.0);
 
 0; 
 succ (pred 0);
 iszero (pred (succ (succ 0))); 
 
-let x=true in x;
+
+/* for untyped lambda calculus */
+
+lambda x. x;
+(lambda x. x) (lambda x. x x);
+
+x/;
+
+x;
+lambda x. x;
+(lambda x. x) (lambda x. x x);
+
+w/;
+a/;
+
+(lambda b.w (lambda a.b a)) (lambda b.b a);
